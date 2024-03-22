bb13444c3bd1cad8d6113b8c8a7f502919ef2aa6
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-03-22 15:45:55 +0100

    add routing and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import "./App.css";
-import Game from "./components/Game";
-import WelcomeScreen from "./components/WelcomeScreen";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "./store/store";
 import { useEffect } from "react";
+import Game from "./components/Game";
+import WelcomeScreen from "./components/WelcomeScreen";
 import HighScoreTable from "./components/HighScoreTable";
 
 function App() {
   const name = useSelector((state: RootState) => state.game.name);
-  const state = useSelector((state:RootState) => state);
+  const state = useSelector((state: RootState) => state);
 
   useEffect(() => {
-    console.log(state)
-  },[state])
+    console.log(state);
+  }, [state]);
 
-  return <>{name ? <Game /> : <HighScoreTable />}</>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={name ? <Navigate to="/game" /> : <WelcomeScreen />}
+          />
+          <Route path="/game" element={name ? <Game /> : <Navigate to="/" />} />
+          <Route path="/highscores" element={<HighScoreTable />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
